ea8d7f4025fda38e0b18695043a0b5a05d320f94
Tetsuo Koyama <tkoyama010@gmail.com>
2022-07-24 08:15:36 +0900

    Create update.sh

---
+++ locale/update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# update transifex pot and local po files
+
+set -ex
+
+# required environment variables
+# SPHINXINTL_TRANSIFEX_USERNAME=tkoyama010
+# SPHINXINTL_TRANSIFEX_PROJECT_NAME=pyvista-tutorial
+
+# pull po files from transifex
+cd `dirname $0`
+sphinx-intl create-transifexrc
+#rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
+sphinx-build -T -b gettext ../pyvista-tutorial/doc pot || true  # will fail on VTK9
+sphinx-build -T -b gettext ../pyvista-tutorial/doc pot || true  # need it again due to all our docstring examples
+sphinx-build -T -b gettext ../pyvista-tutorial/doc pot
+sphinx-intl update-txconfig-resources -p pot -d .
+cat .tx/config
+tx push -s --skip
+rm -Rf ja zh zh_CN zh_Hans zh_TW en_CA en_US de_DE jv nb pl pl_PL ru es uk fa
+tx pull -l ja,zh_CN,zh_Hans,zh_TW,en_CA,en_US,de_DE,jv,nb,pl,pl_PL,ru,es,uk,fa
+git checkout .tx/config
